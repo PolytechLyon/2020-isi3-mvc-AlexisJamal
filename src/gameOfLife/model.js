8af52ef1baee038a2dc753b7cc8d9ed6fe258b5e
AlexisJamal <45990193+AlexisJamal@users.noreply.github.com>
2020-05-28 15:20:16 +0200

    Jeu terminé

--- src/gameOfLife/model.js
+++ src/gameOfLife/model.js
@@ -4,6 +4,7 @@ import {
   DEFAULT_ALIVE_PAIRS,
   RENDER_INTERVAL
 } from "./constants";
+import { drawGame } from "./view";
 
 export class Model {
   constructor() {
@@ -26,13 +27,31 @@ export class Model {
     this.raf = requestAnimationFrame(() => {
       const currentTime = new Date().getTime();
       if (currentTime - date > RENDER_INTERVAL) {
-
+        let nextState = Array.from(new Array(this.height), () =>
+          Array.from(new Array(this.width), () => CELL_STATES.NONE)
+        );
+        for (let i = 0; i < this.width; i++) {
+          for (let j = 0; j < this.width; j++) {
+            nextState[i][j] = this.state[i][j];
+          }
+        }
         for (let i = 0; i < this.width; i++) {
           for (let j = 0; j < this.width; j++) {
             const nbAlive = this.aliveNeighbours(i, j);
             // TODO implement Game of life logic
+            if (
+              this.state[i][j] === CELL_STATES.ALIVE &&
+              nbAlive !== 2 &&
+              nbAlive !== 3
+            ) {
+              nextState[i][j] = CELL_STATES.DEAD;
+            } else if (nbAlive === 3) {
+              nextState[i][j] = CELL_STATES.ALIVE;
+            }
           }
         }
+
+        this.state = nextState;
 
         this.updated();
         this.run(currentTime);
@@ -49,6 +68,8 @@ export class Model {
 
   reset() {
     // TODO
+    this.stop();
+    this.init();
   }
 
   isCellAlive(x, y) {
@@ -56,17 +77,25 @@ export class Model {
       y >= 0 &&
       y < this.height &&
       x < this.height &&
-      this.state[y][x] === CELL_STATES.ALIVE
+      this.state[x][y] === CELL_STATES.ALIVE
       ? 1
       : 0;
   }
   aliveNeighbours(x, y) {
     let number = 0;
     // TODO
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (i !== 0 || j !== 0) {
+          number += this.isCellAlive(x + i, y + j);
+        }
+      }
+    }
     return number;
   }
 
   updated() {
     // TODO update the view
+    drawGame(this);
   }
 }
